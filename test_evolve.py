8f23d82dacec4a020d70e1b65d502d00e748f310
Daniel Reverri <reverri@gmail.com>
2009-07-05 13:35:26 -0700

    Fixed problem with tests

--- test_evolve.py
+++ test_evolve.py
@@ -89,121 +89,121 @@ class TestEvolveBranch(unittest.TestCase):
 		
 		
 class TestEvolveCommit(unittest.TestCase):
-		def setUp(self):
-			self.repo = evolve.Repository()
-			change = {
-				"change":"create",
-				"schema":{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"id":{"type":"string"}
-					}
+	def setUp(self):
+		self.repo = evolve.Repository()
+		change = {
+			"change":"create",
+			"schema":{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"id":{"type":"string"}
 				}
 			}
-			branch = self.repo.branch('master')
-			branch.add(change)
-			branch.commit('test commit')
-			
-		def test_getAncestors(self):
-			branch = self.repo.checkout('master')
-			commit = branch.parent
-			a = commit.getAncestors()
-			self.assertTrue('root' in a)
-			self.assertTrue(branch.parent.commit_id in a)
-			
-		def test_rollback(self):
-			branch = self.repo.checkout('master')
-			commit = branch.parent
-			revlog = commit.rollback('root')
-			self.assertEqual(len(revlog),1)
-			self.assertTrue(revlog[0]['change'] == 'drop')
-			
-		def test_reverse_create(self):
-			change = {
-				"change":"create",
-				"schema":{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"id":{"type":"string"}
-					}
-				}
-			}
-			branch = self.repo.checkout('master')
-			commit = branch.parent
-			reverse_change = commit.reverse(change)
-			self.assertTrue(reverse_change['change'] == 'drop')
-			
-		def test_reverse_drop(self):
-			change = {
-				"change":"create",
-				"schema":{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"id":{"type":"string"}
-					}
-				}
-			}
-			branch = self.repo.branch('reverse_drop','master')
-			commit = branch.parent
-			change1 = commit.reverse(change)
-			branch.add(change1)
-			branch.commit('drop table')
-			commit = branch.parent
-			change2 = commit.reverse(change1)
-			self.assertTrue(change2['change'] == 'create')
-			
-		def test_reverse_alter_add(self):
-			branch = self.repo.branch('reverse_alter_add','master')
-			change = {
-				'change':'alter.add',
-				'schema':{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"name":{"type":"string"}
-					}
-				}
-			}
-			branch.add(change)
-			branch.commit('reverse_alter_add')
-			commit = branch.parent
-			c2 = commit.reverse(change)
-			self.assertTrue(c2['change'] == 'alter.drop')
+		}
+		branch = self.repo.branch('master')
+		branch.add(change)
+		branch.commit('test commit')
 		
-		def test_reverse_alter_modify(self):
-			branch = self.repo.branch('reverse_alter_modify','master')
-			c1 = {
-				'change':'alter.add',
-				'schema':{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"name":{"type":"string"}
-					}
+	def test_getAncestors(self):
+		branch = self.repo.checkout('master')
+		commit = branch.parent
+		a = commit.getAncestors()
+		self.assertTrue('root' in a)
+		self.assertTrue(branch.parent.commit_id in a)
+		
+	def test_rollback(self):
+		branch = self.repo.checkout('master')
+		commit = branch.parent
+		revlog = commit.rollback('root')
+		self.assertEqual(len(revlog),1)
+		self.assertTrue(revlog[0]['change'] == 'drop')
+		
+	def test_reverse_create(self):
+		change = {
+			"change":"create",
+			"schema":{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"id":{"type":"string"}
 				}
 			}
-			c2 = {
-				'change':'alter.modify',
-				'schema':{
-					"id":"person",
-					"type":"object",
-					"properties":{
-						"name":{"type":"string","maxLength":50}
-					}
+		}
+		branch = self.repo.checkout('master')
+		commit = branch.parent
+		reverse_change = commit.reverse(change)
+		self.assertTrue(reverse_change['change'] == 'drop')
+		
+	def test_reverse_drop(self):
+		change = {
+			"change":"create",
+			"schema":{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"id":{"type":"string"}
 				}
 			}
-			branch.add(c1)
-			branch.add(c2)
-			branch.commit('reverse_alter_modify')
-			commit = branch.parent
-			c3 = commit.reverse(c2)
-			self.assertTrue(c3['change'] == 'alter.modify')
-			self.assertFalse('maxLength' in c3['schema']['properties']['name'])
+		}
+		branch = self.repo.branch('reverse_drop','master')
+		commit = branch.parent
+		change1 = commit.reverse(change)
+		branch.add(change1)
+		branch.commit('drop table')
+		commit = branch.parent
+		change2 = commit.reverse(change1)
+		self.assertTrue(change2['change'] == 'create')
+		
+	def test_reverse_alter_add(self):
+		branch = self.repo.branch('reverse_alter_add','master')
+		change = {
+			'change':'alter.add',
+			'schema':{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"name":{"type":"string"}
+				}
+			}
+		}
+		branch.add(change)
+		branch.commit('reverse_alter_add')
+		commit = branch.parent
+		c2 = commit.reverse(change)
+		self.assertTrue(c2['change'] == 'alter.drop')
+	
+	def test_reverse_alter_modify(self):
+		branch = self.repo.branch('reverse_alter_modify','master')
+		c1 = {
+			'change':'alter.add',
+			'schema':{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"name":{"type":"string"}
+				}
+			}
+		}
+		c2 = {
+			'change':'alter.modify',
+			'schema':{
+				"id":"person",
+				"type":"object",
+				"properties":{
+					"name":{"type":"string","maxLength":50}
+				}
+			}
+		}
+		branch.add(c1)
+		branch.add(c2)
+		branch.commit('reverse_alter_modify')
+		commit = branch.parent
+		c3 = commit.reverse(c2)
+		self.assertTrue(c3['change'] == 'alter.modify')
+		self.assertFalse('maxLength' in c3['schema']['properties']['name'])
 			
-class TestEvolveCommit(unittest.TestCase):
+class TestEvolveRepository(unittest.TestCase):
 	def setUp(self):
 		self.repo = evolve.Repository()
 		change = {
